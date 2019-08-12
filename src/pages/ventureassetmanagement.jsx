3a8d9bb21d3c2b4e9cd8a4a57da4be0825623fb3
philippzach <mail@philippzach.com>
2019-08-12 15:22:41 +0200

    Typos

--- src/pages/ventureassetmanagement.jsx
+++ src/pages/ventureassetmanagement.jsx
@@ -1,59 +1,67 @@
 import React from 'react';
 import styled from 'react-emotion';
 import { Layout, Wrapper, Navbar, Title } from 'components';
-//import Background from '../../static/investorclub.jpg';
+// import Background from '../../static/investorclub.jpg';
 import '../styles/pages/investorclub.css';
-import {Link} from "gatsby";
+import { Link } from 'gatsby';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import Strebel from '../../static/hanspeter_strebel.jpg';
 import Urs from '../../static/urs_wietlisbach.jpg';
 import Chris from '../../static/chris_wildmoser.jpg';
 import SSUC from '../images/ssuc.svg';
 import Seo from '../components/SEO/index';
-import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 const Test = styled.div`
-    position: relative;
-    overflow: hidden;
-    vertical-align: middle;
-    max-height: 450px;
-`
+  position: relative;
+  overflow: hidden;
+  vertical-align: middle;
+  max-height: 450px;
+`;
 const Background = styled.div`
-background-color: #ebecee;
-`
+  background-color: #ebecee;
+`;
 const Header = styled.span`
-font-size: 50px;
-font-family: poppins;
-font-weight: bold;
-line-height: 1em;
-`
+  font-size: 50px;
+  font-family: poppins;
+  font-weight: bold;
+  line-height: 1em;
+`;
 const Paragraph = styled.p`
-font-family: roboto;
-font-weight: 200;
-font-size: 1em;
-padding-top: 1em;
-max-width: 18em;
-`
+  font-family: roboto;
+  font-weight: 200;
+  font-size: 1em;
+  padding-top: 1em;
+  max-width: 18em;
+`;
 
 const Venture = ({ data }) => (
   <Layout>
-    <Seo title="SWISS STARTUP GROUP – Venture Asset Mangement" desc="The Swiss Startup Group has founded Swiss Startup Capital AG as a Venture Asset Management division. Direct Investments - Indirect Investments - Corporate Venture Capital." />
+    <Seo
+      title="SWISS STARTUP GROUP – Venture Asset Mangement"
+      desc="The Swiss Startup Group has founded Swiss Startup Capital AG as a Venture Asset Management division. Direct Investments - Indirect Investments - Corporate Venture Capital."
+    />
     <Navbar />
-    <Test> 
+    <Test>
       <Img fluid={data.investorclub.childImageSharp.fluid} className="w-100" />
     </Test>
     <Wrapper style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
       <Title>About</Title>
       <div className="flex-ns mt5 mb6">
         <div className="w-100 w-third-ns flex items-center justify-center justify-start-ns ">
-          <img src={SSUC} alt="Swiss Startup Capital Logo" width="300px"/>
+          <img src={SSUC} alt="Swiss Startup Capital Logo" width="300px" />
         </div>
         <div className="w-100 w-two-thirds-ns pl0 pl4-ns pt4 tc tl-ns">
           <h2 className="lh-copy ttu headingcolorshadow">Venture Asset Management</h2>
           <p className="lh-copy b">
-          In the past 5 years investment in startups in Switzerland have gathered a lot of interest, which has led to a much more professional way of managing venture investments. Today venture investments have become a separate asset class for qualified investors called Venture Asset Management. 
+            In the past 5 years investments in startups in Switzerland have gathered a lot of interest, which has led to
+            a much more professional way of managing venture investments. Today venture investments have become a
+            separate asset class for qualified investors called Venture Asset Management.
           </p>
-          <p className="lh-copy">Therefore the Swiss Startup Group has entrusted the newly founded Swiss Startup Capital AG who represents a fully dedicated Venture Asset Management offering in three divisons: Direct Investments - Indirect Investments - Corporate Venture Capital.
+          <p className="lh-copy">
+            Therefore the Swiss Startup Group has entrusted the newly founded Swiss Startup Capital AG who represents a
+            fully dedicated Venture Asset Management offering in three divisons: Direct Investments - Indirect
+            Investments - Corporate Venture Capital.
           </p>
         </div>
       </div>
@@ -61,84 +69,130 @@ const Venture = ({ data }) => (
     </Wrapper>
     <Background>
       <Wrapper>
-        <h3 className="mw6 lh-copy pt4">
-        A team of dedicated professionals committed to exceeding your expectations
-        </h3>
+        <h3 className="mw6 lh-copy pt4">A team of dedicated professionals committed to exceeding your expectations</h3>
         <div className="testimonialmobile">
           <article className="mobile mw6  pa3 pa4-ns mv3">
             <div className="tc">
-            <Img fixed={data.max.childImageSharp.fixed} alt="Swiss Startup Capital Team" className="shadow-5"/>
+              <Img fixed={data.max.childImageSharp.fixed} alt="Swiss Startup Capital Team" className="shadow-5" />
               <h4 className="pt4 mb1">Max Meister</h4>
             </div>
-            <p className="tc measure center f6 black-70">CEO
-            </p>
+            <p className="tc measure center f6 black-70">CEO</p>
           </article>
           <article className="mobile mw6  pa3 pa4-ns mv3">
             <div className="tc">
-            <Img fixed={data.philipp.childImageSharp.fixed} alt="Swiss Startup Capital Team" className="shadow-5"/>
+              <Img fixed={data.philipp.childImageSharp.fixed} alt="Swiss Startup Capital Team" className="shadow-5" />
               <h4 className="pt4 mb1">Philipp Steinberger</h4>
             </div>
-            <p className="tc measure center f6 black-70">Head of Investor Club
-            </p>
+            <p className="tc measure center f6 black-70">Head of Investor Club</p>
           </article>
           <article className="mobile mw6  pa3 pa4-ns mv3">
             <div className="tc">
-            <Img fixed={data.flavio.childImageSharp.fixed} alt="Swiss Startup Capital Team" className="shadow-5"/>
+              <Img fixed={data.flavio.childImageSharp.fixed} alt="Swiss Startup Capital Team" className="shadow-5" />
               <h4 className="pt4 mb1">Flavio Bianchi</h4>
             </div>
-            <p className="tc  measure center f6 black-70">Associate
-            </p>
+            <p className="tc  measure center f6 black-70">Associate</p>
           </article>
         </div>
       </Wrapper>
     </Background>
     <Wrapper>
-    <Title className="mv5">Services</Title>
-    <div style={{ paddingTop: '2rem', paddingBottom: '4rem' }}>
-      <div className="row mt4">
-        <div className="w-100 w-50-ns">
-          <Header>Investments<br/>& Support</Header>
-          <Paragraph>through our proprietary dealflow, due diligence process and portfolio support we create value for our companies and investors.</Paragraph>
-        </div>
-        <div className="w-100 w-50-ns">
-          <h3>1. Direct Investments</h3>
-          <p className="lh-copy b">In the area of direct investments, we present qualified investors a carefully selected pipeline of promising investment opportunities and support individual investment decisions with a professional due diligence.</p>
-          <p className="lh-copy">At the request of investors, we accompany the investments, actively support them and regularly report on their development. With this competent offer, we address experienced private investors as well as multi-family offices and asset managers and banks who would like to give their customers professional access to an attractive venture asset class.</p>
-          <p className="lh-copy">In the form of managed accounts, we offer our vast venture investment experience to investors who already own a portfolio of venture investments. This service is aimed in particular to clients who already own a venture investment portfolio. We analyze the individual portfolio companies, manage the most important investments and report to the investor on a regular basis. Like this, we support our clients so they can focus on their other core asset classes, creating value efficiently.</p>
-          <Link to="/investorclub">
-          <div className="button -dark center">Join the Investor Club</div>
-        </Link>
-        </div>
-      </div>
-      <div className="row mv6">
-      <div className="w-100 w-50-ns">
-       <Header>SSUG Flagship<br/> Fund I</Header>
-       <Paragraph>The first fund will start operations in Q3 2019.</Paragraph>
-      </div>
-      <div className="w-100 w-50-ns">
-        <h3>2. Indirect Investments</h3>
-        <p className="lh-copy">In order to invest into new and promising growth companies, but also to conduct follow-on investments in our existing portfolio, the Swiss Startup Group has decided to set up its own fund - The SSUG Flagship Fund I, will be operational as of Q3 2019.</p>
-        <p className="lh-copy">This fund is dedicated to qualified private and institutional investors who want to invest in venture with an indirect investment approach and on a discretionary base. It is a suitable solution for all investors who wish to delegate the investment decisions.</p>     
-      </div>
-      </div>
-      <div className="row mb4">
+      <Title className="mv5">Services</Title>
+      <div style={{ paddingTop: '2rem', paddingBottom: '4rem' }}>
+        <div className="row mt4">
           <div className="w-100 w-50-ns">
-          <Header>Corporate<br/>Venture<br/>Capital</Header>
-          <Paragraph>will be a major topic of interest for the Swiss Startup Capital AG.</Paragraph>
+            <Header>
+              Investments
+              <br />& Support
+            </Header>
+            <Paragraph>
+              through our proprietary dealflow, due diligence process and portfolio support we create value for our
+              companies and investors.
+            </Paragraph>
+          </div>
+          <div className="w-100 w-50-ns">
+            <h3>1. Direct Investments</h3>
+            <p className="lh-copy b">
+              In the area of direct investments, we present qualified investors a carefully selected pipeline of
+              promising investment opportunities and support individual investment decisions with a professional due
+              diligence.
+            </p>
+            <p className="lh-copy">
+              At the request of investors, we accompany the investments, actively support them and regularly report on
+              their development. With this competent offer, we address experienced private investors as well as
+              multi-family offices and asset managers and banks who would like to give their customers professional
+              access to an attractive venture asset class.
+            </p>
+            <p className="lh-copy">
+              In the form of managed accounts, we offer our vast venture investment experience to investors who already
+              own a portfolio of venture investments. We analyze the individual portfolio companies, manage the most
+              important investments and report to the investor on a regular basis. Like this, we support our clients so
+              they can focus on their other core asset classes, creating value efficiently.
+            </p>
+            <Link to="/investorclub">
+              <div className="button -dark center">Join the Investor Club</div>
+            </Link>
+          </div>
+        </div>
+        <div className="row mv6">
+          <div className="w-100 w-50-ns">
+            <Header>
+              SSUG Flagship
+              <br /> Fund I
+            </Header>
+            <Paragraph>The first fund will start operations in Q3 2019.</Paragraph>
+          </div>
+          <div className="w-100 w-50-ns">
+            <h3>2. Indirect Investments</h3>
+            <p className="lh-copy">
+              In order to invest into new and promising growth companies, but also to conduct follow-on investments in
+              our existing portfolio, the Swiss Startup Group has decided to set up its own fund - The SSUG Flagship
+              Fund I, will be operational as of Q3 2019.
+            </p>
+            <p className="lh-copy">
+              This fund is dedicated to qualified private and institutional investors who want to invest in venture with
+              an indirect investment approach and on a discretionary base. It is a suitable solution for all investors
+              who wish to delegate the investment decisions.
+            </p>
+          </div>
+        </div>
+        <div className="row mb4">
+          <div className="w-100 w-50-ns">
+            <Header>
+              Corporate
+              <br />
+              Venture
+              <br />
+              Capital
+            </Header>
+            <Paragraph>will be a major topic of interest for the Swiss Startup Capital AG.</Paragraph>
           </div>
           <div className="w-100 w-50-ns">
             <h3>3. Corporate Venturing</h3>
-            <p className="lh-copy">In recent years, the Swiss Startup Group has been increasingly mandated by its partners to manage and accompany transactions for their investments. In particular, these included spin-offs, the setting-up of joint ventures and the preparation and execution of exit transactions. Swiss Startup Capital AG has experienced experts at its disposal who can support clients going forward.</p>
-            <p className="lh-copy"><a style={{color: "inherit", textDecoration: "underline"}}href="https://ccvs.ch/">Corporate Venture Capital</a> will also be a core focus for Swiss Startup Capital AG. We support companies in setting up and implementing their own structures (internal - external - fund structures).</p>
+            <p className="lh-copy">
+              In recent years, the Swiss Startup Group has been increasingly mandated by its partners to manage and
+              accompany transactions for their investments. In particular, these included spin-offs, the setting-up of
+              joint ventures and the preparation and execution of exit transactions. Swiss Startup Capital AG has
+              experienced experts at its disposal who can support clients going forward.
+            </p>
+            <p className="lh-copy">
+              <a style={{ color: 'inherit', textDecoration: 'underline' }} href="https://ccvs.ch/">
+                Corporate Venture Capital
+              </a>{' '}
+              will also be a core focus for Swiss Startup Capital AG. We support companies in setting up and
+              implementing their own structures (internal - external - fund structures).
+            </p>
           </div>
-      </div>
-      <p className="mw6 center tc lh-copy pt5 b">We are convinced that these ingredients make up an exclusive Swiss pipeline of early-stage investment opportunities here in Switzerland.</p>
-      {/* <div className="tc mv3">
+        </div>
+        <p className="mw6 center tc lh-copy pt5 b">
+          We are convinced that these ingredients make up an exclusive Swiss pipeline of early-stage investment
+          opportunities here in Switzerland.
+        </p>
+        {/* <div className="tc mv3">
         <Link to="/investorclub">
           <div className="button -dark center">Join the Investor Club</div>
         </Link>
       </div> */}
-    </div>
+      </div>
     </Wrapper>
   </Layout>
 );
@@ -148,43 +202,32 @@ export default Venture;
 export const query = graphql`
   query {
     investorclub: file(relativePath: { eq: "images/investorclub.jpg" }) {
-        childImageSharp {
-          fluid(
-            maxWidth: 1500
-            traceSVG: { background: "#fff", color: "lightgrey" }
-          ) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
+      childImageSharp {
+        fluid(maxWidth: 1500, traceSVG: { background: "#fff", color: "lightgrey" }) {
+          ...GatsbyImageSharpFluid_tracedSVG
         }
+      }
     }
     max: file(relativePath: { eq: "images/max-ssuc.jpg" }) {
       childImageSharp {
-        fixed(
-          height: 350
-          traceSVG: { background: "#fff", color: "lightgrey" }
-        ) {
+        fixed(height: 350, traceSVG: { background: "#fff", color: "lightgrey" }) {
           ...GatsbyImageSharpFixed_tracedSVG
         }
       }
-  }
-  philipp: file(relativePath: { eq: "images/philipp-ssuc.jpg" }) {
-    childImageSharp {
-      fixed(
-        height: 350
-        traceSVG: { background: "#fff", color: "lightgrey" }
-      ) {
-        ...GatsbyImageSharpFixed_tracedSVG
+    }
+    philipp: file(relativePath: { eq: "images/philipp-ssuc.jpg" }) {
+      childImageSharp {
+        fixed(height: 350, traceSVG: { background: "#fff", color: "lightgrey" }) {
+          ...GatsbyImageSharpFixed_tracedSVG
+        }
       }
     }
-}
-flavio: file(relativePath: { eq: "images/flavio-ssuc.jpg" }) {
-  childImageSharp {
-    fixed(
-      height: 350
-      traceSVG: { background: "#fff", color: "lightgrey" }
-    ) {
-      ...GatsbyImageSharpFixed_tracedSVG
+    flavio: file(relativePath: { eq: "images/flavio-ssuc.jpg" }) {
+      childImageSharp {
+        fixed(height: 350, traceSVG: { background: "#fff", color: "lightgrey" }) {
+          ...GatsbyImageSharpFixed_tracedSVG
+        }
+      }
     }
   }
-}
-  }`
+`;
